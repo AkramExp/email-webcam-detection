04d223f8a0ac44bcadde5198ca87909472c785ed
Akram Ali <akram.exp02@gmail.com>
2023-06-26 21:35:02 +0530

    add object detect feature

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 import time
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(2)
 time.sleep(1)
-first_frame= None
+first_frame = None
 while True:
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -14,10 +14,16 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 
-    thresh_frame = cv2.threshold(delta_frame, 20, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=3)
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0),3)
 
-    cv2.imshow("My Video", dil_frame)
+    cv2.imshow("My Video", frame)
 
     key = cv2.waitKey(1)
 
