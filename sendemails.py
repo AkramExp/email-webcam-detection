d33cf2f4569539115e71e34742cf3f8d23343dab
Akram Ali <akram.exp02@gmail.com>
2023-06-27 17:38:06 +0530

    function complete

--- sendemails.py
+++ sendemails.py
@@ -1,2 +1,23 @@
-def sendEmail():
-    print("sent mail")
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+PASSWORD = "daofpapzhirvlkqb"
+USERNAME = "its.akram246@gmail.com"
+RECEIVER = "its.akram246@gmail.com"
+
+def sendEmail(image_path):
+    email_message = EmailMessage()
+    email_message["subject"] = "new customer showed up"
+    email_message.set_content("a new customer")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(USERNAME, PASSWORD)
+    gmail.sendmail(USERNAME, RECEIVER, email_message.as_string())
+    gmail.quit()
